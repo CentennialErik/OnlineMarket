21865f7a8c6261ac8f019dac0c4d91a0f216504c
CentennialErik <144242402+CentennialErik@users.noreply.github.com>
2023-11-30 17:59:20 -0500

    Update Product.jsx
    Added Circular Loading, modified useEffect to use async/await

--- OnlineMarket/client/product/Product.jsx
+++ OnlineMarket/client/product/Product.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 // import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
+import CirculatProgress from '@material-ui/core/CirculatProgress';
 
 
 const myBox = {
@@ -10,22 +11,35 @@ const myBox = {
 export default function Products() {
 
   const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
   useEffect(() => {
     // Fetch product data from an API or database
-    fetch('/api/product')
-      .then(response => response.json())
-      .then(data => setProducts(data))
-      .catch(error => console.log(error));
+    const fetchProducts = async () => {
+      try {
+        const response = await fetch ('/api/prudct');
+        const data = await response.json();
+        setProducts(data);
+      }
+      catch (error) {
+        console.error('Error fetching products:', error);
+      } finally {
+        setLoading(false);
+      };
+      fetchProducts ();
   }, []);
   return (
     <div>
+      {loading ? (
+      <CirculatProgress stlye={{ margin: '50px' }} />
+      ) : (
       {products.map((product) => (
         <div style={myBox} key={product.id}>
           <h1>{product.name}</h1>
           <p>{product.description}</p>
           <p>${product.price}</p>
         </div>
-      ))}
+      ))
+    )}
     </div>
   );
 }
